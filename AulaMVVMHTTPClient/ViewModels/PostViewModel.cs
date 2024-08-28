8a462d121d189d62ffdea102759f0c46fd952134
ZetzGold <tetorrrique@gmail.com>
2024-08-28 15:21:30 -0300

    HTTP

---
+++ AulaMVVMHTTPClient/ViewModels/PostViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AulaMVVMHTTPClient.Models;
+using AulaMVVMHTTPClient.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace AulaMVVMHTTPClient.ViewModels
+{
+    public partial class PostViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        List<Post> posts;
+        public PostService postsService;
+        private ICommand getPostsCommand; 
+
+        PostViewModel() { 
+            PostService postsService = new PostService();
+            getPostsCommand = new Command(getPosts);
+        }
+
+        public async void getPosts()
+        {
+            //Buscar os dados da API
+            posts = await postsService.GetPosts();
+        }
+    }
+}
---
+++ AulaMVVMHTTPClient/Views/PostView.xaml.cs
@@ -0,0 +1,9 @@
+namespace AulaMVVMHTTPClient.Views;
+
+public partial class PostView : ContentPage
+{
+	public PostView()
+	{
+		InitializeComponent();
+	}
+}
